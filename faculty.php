50aba3be9bb062174d9dcbff08a92e8d435a7035
Ziithe Ewen Hiwa <z.hiwa@alustudent.com>
2021-12-05 10:04:02 +0200

    Faculty Page

--- faculty.php
+++ faculty.php
@@ -1,16 +1,139 @@
+<?php
+
+session_start();
+require 'includes/sanitize.php';
+
+include("header.php");
+include("connect.php");
+
+if(isset($_POST['borrow'])){
+  $id = sanitize(trim($_POST['id']));
+  if(isset($_SESSION['login_user'])){
+      $result = mysqli_query($dbhandle, "INSERT INTO issue_books(email, BookID, Approve_Status, Issue_Date, Return_Date) values ('$_SESSION[login_user]', '$id', ' ', ' ', ' ');");
+      if($result===TRUE)
+        {
+            echo "<script>alert('Book borrowing request submitted!');
+            window.location='faculty.php';
+            </script>";
+        }      
+        else
+        {
+            echo"Sorry!Book could not be borrowed. Try again later!";
+        }
+
+  }
+  else {
+    ?>
+      <script type='text/javascript'>
+        alert("Kindly login first!")
+      </script>
+    <?php
+  }
+}
+
+?>
+
 <html>
   <head>
-    <title> Faculty Page </title>
+    <title> Welcome to the ALU Library </title>
+    <link rel="stylesheet" href="style.css">
+
  <head>
-  <style>
-    body {
-   font-family:arial;
-   color:yellow;
-  };
- </style>
-<body><br>
- <h1 align="center">
-  Welcome To Faculty Page 
-  </h1>
- </body>
- </html>
+  
+<body>
+<ul>
+  <li><a class="active" href="faculty.php">Home</a></li>
+  <li><a href="#news">My Borrowed Books</a></li>
+  <li><a href="#news">Profile</a></li>
+  <li><a href="#about">Logout</a></li>
+</ul>
+
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+      <div class="row">
+        <h3>Hello, <?php echo $_SESSION["login_user"]; ?></h3>
+			  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pull-right">
+			  	<form method="post" action="faculty.php" enctype="multipart/form-data">
+			  		<div class="input-group pull-right">
+				      <span class="input-group-addon">
+				        <button type="submit" class="btn btn-success" name="search">Search</button> 
+				      </span>
+				      <input type="text" class="form-control" name="text" required>
+			      </div>
+			  	</form>
+			    
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>BookTitle</th>
+            <th>Author</th>
+            <th>Date Published</th>
+            <th>Available</th>
+            <th>Category</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <?php
+
+        if (isset($_POST['search'])) {
+          $query = mysqli_query($dbhandle, "SELECT * FROM books where BookTitle like '%$_POST[text]%'");
+
+          if (mysqli_num_rows($query)==0) {
+            echo "Sorry. Books not available. Try searching again!";
+          }
+          else {
+            while($row = mysqli_fetch_array($query)){ ?>
+              <tbody class="table-elements">
+                <td><?php echo $row['BookTitle']; ?></td>
+                <td><?php echo $row['Author']; ?></td>
+                <td><?php echo $row['Date_Published']; ?></td>
+                <td><?php echo $row['Available_Copies']; ?></td>
+                <td><?php echo $row['Category']; ?></td>
+                <form method = "POST" action="faculty.php">
+                  <input type='hidden' value="<?php echo $row['BookID']; ?>" name='id'>
+                  <td>
+                    <button name='borrow' type='submit' value='Borrow' class='btn btn-warning'>Borrow</button>
+                  </td>
+                </form>
+              </tbody>
+            <?php
+            }
+        }
+      }
+      
+      else{
+          $sql2 ="SELECT * from books";
+          $query2 = mysqli_query($dbhandle, $sql2);
+          $counter = 1;
+          while($row = mysqli_fetch_array($query2)){ ?>
+            <tbody class="table-elements">
+              <td><?php echo $row['BookTitle']; ?></td>
+              <td><?php echo $row['Author']; ?></td>
+              <td><?php echo $row['Date_Published']; ?></td>
+              <td><?php echo $row['Available_Copies']; ?></td>
+              <td><?php echo $row['Category']; ?></td>
+              <form method = "POST" action="faculty.php">
+                <input type='hidden' value="<?php echo $row['BookID']; ?>" name='id'>
+                <td>
+                  <button name='borrow' type='submit' value='Borrow' class='btn btn-warning'>Borrow</button>
+                </td>
+              </form>
+            </tbody>
+          <?php
+          }
+        }
+        ?>
+        </table>
+
+          
+			</div>
+      </div>
+
+      
+
+  </div>
+    
+</body>
+</html>
